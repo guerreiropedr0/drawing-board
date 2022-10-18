d54db35fe0f029f148048ed4bd0b34bfb96d9129
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-18 15:34:01 +0100

    Add painting and tools (pencil for now)

--- script.js
+++ script.js
@@ -4,7 +4,58 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const mouse = {
+  x: null,
+  y: null,
+}
+
+const pencil = {
+  isPainting: false,
+  x: mouse.x,
+  y: mouse.y,
+  color: 'white',
+  width: 10,
+}
+
+let currentTool = pencil;
+
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 })
+
+canvas.addEventListener('mousemove', (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+})
+
+const updateToolPositions = (tool) => {
+  const updatedTool = tool;
+
+  updatedTool.x = mouse.x;
+  updatedTool.y = mouse.y;
+
+  return updatedTool;
+}
+
+const paint = (tool) => {
+  if (tool.isPainting) {
+    updateToolPositions(tool);
+    ctx.fillStyle = tool.color;
+    ctx.beginPath();
+    ctx.arc(tool.x, tool.y, tool.width, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+canvas.addEventListener('mousedown', () => {
+  currentTool.isPainting = true;
+})
+
+canvas.addEventListener('mouseup', () => {
+  currentTool.isPainting = false;
+})
+
+canvas.addEventListener('mousemove', () => {
+  paint(currentTool);
+});
