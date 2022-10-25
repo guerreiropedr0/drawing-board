addd9f2d60485319d1118fe62216b0a2704291d5
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-25 11:51:35 +0100

    Add styling and pencil modal with width feature

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ import { mouse, pencil, eraser } from "./tools.js";
 
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+
+const controlPanel = document.querySelector('#control_panel');
+const pencilBtn = document.querySelector('#pencil');
+const pencilModal = document.querySelector('#pencil_modal');
 const colorPicker = document.querySelector('#color_picker');
+const pencilWidthIncrement = document.querySelector('#pencil_width_increment');
+const pencilWidth = document.querySelector('#pencil_width');
+const pencilWidthDecrement = document.querySelector('#pencil_width_decrement');
 const eraserBtn = document.querySelector('#eraser');
-const clearAllBtn = document.querySelector('#clear_all');
+// const clearAllBtn = document.querySelector('#clear_all');
 const fillColor = document.querySelector('#fill_color');
 const DEFAULT_COLOR_PICKER_COLOR = '#000000';
 
 colorPicker.value = DEFAULT_COLOR_PICKER_COLOR;
 
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = window.innerWidth - 150;
+canvas.height = window.innerHeight - 100;
+controlPanel.style.width = canvas.width + 'px';
 
 let currentTool = pencil;
 
@@ -62,11 +70,41 @@ colorPicker.addEventListener('input', ({ target }) => {
 
 eraserBtn.addEventListener('click', () => {
   currentTool = eraser;
+  eraserBtn.classList.add('selected');
+  pencilBtn.classList.remove('selected');
 })
 
-clearAllBtn.addEventListener('click', () => {
-  currentTool.clearAll();
+pencilBtn.addEventListener('mouseover', () => {
+  pencilModal.style.display = 'block';
 })
+
+pencilModal.addEventListener('mouseleave', () => {
+  pencilModal.style.display = 'none';
+})
+
+pencilBtn.addEventListener('click', () => {
+  currentTool = pencil;
+  pencilBtn.classList.add('selected');
+  eraserBtn.classList.remove('selected');
+})
+
+pencilWidthDecrement.addEventListener('click', () => {
+  if (currentTool.width > 1) {
+    currentTool.width -= 1;
+    pencilWidth.textContent = currentTool.width;
+  }
+})
+
+pencilWidthIncrement.addEventListener('click', () => {
+  if (currentTool.width < 50) {
+    currentTool.width += 1;
+    pencilWidth.textContent = currentTool.width;
+  }
+})
+
+// clearAllBtn.addEventListener('click', () => {
+//   currentTool.clearAll();
+// })
 
 fillColor.addEventListener('click', () => {
   currentTool.fill();
